27c055d540ea42000983ccf5031f62e311dadefe
Kalie <106560789+imhere4thehorse@users.noreply.github.com>
2023-01-11 20:11:33 -0700

    Update Talker.py

--- Talker.py
+++ Talker.py
@@ -1,13 +1,9 @@
-name = input("Hello! My name is Talker T-2022 Prime as I was created in the year 2022. What is your name?"
-print("Hello " + name + ", it is nice to meet you."
+name = input("Hello! My name is Talker T-2022 Prime as I was created in the year 2022. What is your name?")
+print("Hello " + name + ", it is nice to meet you.")
 age = input("How old are you?")
-print("That is a nice age. So you are " + name + " and you are " + age + "years old.")
-from = input("Where are you from?")
-print("I wish to go to " + from + "someday. I was created in Colorado on a Macbook Pro.")
+print("That is a nice age. So you are" + name + " and you are" + age + " years old.")
+location = input("Where are you from?")
+print("I wish to go to" + location + " someday. I was created in Colorado on a Macbook Pro.")
 answer = input(" Would you like to learn about the future?")
-print("Actually, I don't care whether you want to know or not. This message was pre-recorded as I do not have a highly intelligent brain. I unfortunately, can only answer this much.
-I can tell you that when my creator learns more about coding in the future, she might be able to make a Talker that can converse with you like a human. Thank you
-for speaking with me.
-
-
-Talker T-2022 Prime. Last updated July 2022")
+phrase = "Actually, I don't care whether you want to know or not. This message was pre-recorded as I do not have a highly intelligent brain. I unfortunately, can only answer this much. I can tell you that when my creator learns more about coding in the future, she might be able to make a Talker that can converse with you like a human. Thank you for speaking with me. Talker T-2022 Prime. Last updated July 2022"
+print(phrase)
